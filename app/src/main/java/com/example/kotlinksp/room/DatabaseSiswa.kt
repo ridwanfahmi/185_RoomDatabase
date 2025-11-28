c8f80edc510d627d4889235bb81f7a0f84f9e360
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 09:53:55 +0700

    membuat abstract class DatabaseSiswa

---
+++ app/src/main/java/com/example/kotlinksp/room/DatabaseSiswa.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinksp.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase()
