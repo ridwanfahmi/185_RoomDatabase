a51d1a6c7a4c678e0ce4d68259633cb72226e2e4
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 09:39:38 +0700

    menambahkan variable id pada data clas siswa

--- app/src/main/java/com/example/kotlinksp/room/Siswa.kt
+++ app/src/main/java/com/example/kotlinksp/room/Siswa.kt
@@ -1,8 +1,13 @@
 package com.example.kotlinksp.room
 
-import androidx.room.vo.Entity
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 @Entity(tableName = "tblSiswa")
 data class Siswa(
+    @PrimaryKey(autoGenerate =  true)
+    val id : Int = 0,
+
 
 )
