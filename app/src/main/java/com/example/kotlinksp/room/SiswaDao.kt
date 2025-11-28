b48132d6ea9e3413d932da451c12aaf389b3c342
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 10:10:30 +0700

    revisi SiswaDao, merubah inser menjadi insert

--- app/src/main/java/com/example/kotlinksp/room/SiswaDao.kt
+++ app/src/main/java/com/example/kotlinksp/room/SiswaDao.kt
@@ -13,5 +13,5 @@ interface SiswaDao {
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun inser(siswa: Siswa)
+    suspend fun insert(siswa: Siswa)
 }
