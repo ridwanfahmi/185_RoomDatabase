f59625c5122ff7b47877bcbecc01d71f886c0186
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 13:40:48 +0700

    menambahkan objek DestinasiEntry sebagai implementasi DestinasiNavigasi untuk rute entry siswa

---
+++ app/src/main/java/com/example/kotlinksp/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinksp.view.route
+
+import com.example.kotlinksp.R
+import com.example.kotlinksp.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val tileRes = R.string.entry_siswa
+}
