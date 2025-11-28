b15b4db7d19a5c5d8bc2c947ad0d592bbb0b15b5
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 14:11:13 +0700

    menambahkan HomeViewModel untuk menampilkan daftar siswa dengan StateFlow dan pengelolaan state menggunakan coroutine

---
+++ app/src/main/java/com/example/kotlinksp/viewmodel/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinksp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kotlinksp.repositori.RepositoriSiswa
+import com.example.kotlinksp.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+}
