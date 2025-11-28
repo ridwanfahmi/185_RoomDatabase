01ee1a8af96b4bb6c26dc9767502664f6b08f550
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 09:55:55 +0700

    menambahkan deklarasi siswaDao dan setup singleton Instance pada DatabaseSiswa

--- app/src/main/java/com/example/kotlinksp/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/kotlinksp/room/DatabaseSiswa.kt
@@ -4,4 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
-abstract class DatabaseSiswa : RoomDatabase()
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+
+    }
+}
