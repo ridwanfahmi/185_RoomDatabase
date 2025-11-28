9d149765b49a48fcd8fb8330bddec6828280d593
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 10:11:42 +0700

    mengimplementasikan OfflineRepositoriSiswa sebagai repository lokal menggunakan SiswaDao untuk stream data dan insert siswa

--- app/src/main/java/com/example/kotlinksp/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/kotlinksp/repositori/RepositoriSiswa.kt
@@ -1,9 +1,17 @@
 package com.example.kotlinksp.repositori
 
 import com.example.kotlinksp.room.Siswa
+import com.example.kotlinksp.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 }
