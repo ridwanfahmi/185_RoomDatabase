0dce93a427fe8dd84fcc0d68f0c3a764105d9a09
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 10:15:50 +0700

    menambahkan ContainerDataApp sebagai implementasi ContainerApp untuk menyediakan RepositoriSiswa menggunakan database lokal

--- app/src/main/java/com/example/kotlinksp/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/kotlinksp/repositori/ContainerApp.kt
@@ -1,5 +1,17 @@
 package com.example.kotlinksp.repositori
 
+import androidx.room.processor.Context
+import com.example.kotlinksp.room.DatabaseSiswa
+
 interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private  val context: Context):
+        ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
 }
