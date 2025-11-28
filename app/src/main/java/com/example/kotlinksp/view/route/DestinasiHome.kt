a07965f09ed49b993d7bf4253314fc5ed4ab15ff
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-28 13:52:38 +0700

    menambahkan DestinasiHome sebagai rute navigasi utama dengan title resource aplikasi

---
+++ app/src/main/java/com/example/kotlinksp/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinksp.view.route
+
+import com.example.kotlinksp.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val tileRes = R.string.app_name
+}
