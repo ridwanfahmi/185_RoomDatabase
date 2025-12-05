bce9c3f16c254fbfd62b85ffb2bad1ca37e4a792
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-05 16:13:25 +0700

    menambahkan initializer detailviewmodel di PenyediaViewModel.kt

--- app/src/main/java/com/example/kotlinksp/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kotlinksp/viewmodel/provider/PenyediaViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.kotlinksp.viewmodel.provider
 
-import android.app.Application
+
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kotlinksp.repositori.AplikasiSiswa
+import com.example.kotlinksp.viewmodel.DetailViewModel
+
 import com.example.kotlinksp.viewmodel.EntryViewModel
 import com.example.kotlinksp.viewmodel.HomeViewModel
 
@@ -18,12 +21,17 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+
     }
 }
 
-/**
- * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
- * instance dari [AplikasiSiswa].
- */
+
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
