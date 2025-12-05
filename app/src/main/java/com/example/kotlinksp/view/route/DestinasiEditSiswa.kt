bb045c51ab57a6b6c4309e95cc96dd91d89292c5
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-05 16:10:16 +0700

    menambahkan DestinasiEditSiswa.kt

---
+++ app/src/main/java/com/example/kotlinksp/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinksp.view.route
+
+import com.example.kotlinksp.R
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
