4fbff66904631a4435d809c159422fcc30b60969
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-05 16:13:45 +0700

    menambahkan initializer editviewmodel di PenyediaViewModel.kt

--- app/src/main/java/com/example/kotlinksp/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kotlinksp/viewmodel/provider/PenyediaViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.kotlinksp.viewmodel.provider
 
-
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
@@ -8,7 +7,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kotlinksp.repositori.AplikasiSiswa
 import com.example.kotlinksp.viewmodel.DetailViewModel
-
+import com.example.kotlinksp.viewmodel.EditViewModel
 import com.example.kotlinksp.viewmodel.EntryViewModel
 import com.example.kotlinksp.viewmodel.HomeViewModel
 
@@ -28,7 +27,12 @@ object PenyediaViewModel {
             )
         }
 
-
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
     }
 }
 
