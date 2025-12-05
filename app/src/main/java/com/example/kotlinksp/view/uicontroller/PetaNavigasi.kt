6704142d7acde26db958aa7caccdcf5fb52c70fa
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-12-05 16:14:22 +0700

    menambahkan banyak komponen di petanavigasi

--- app/src/main/java/com/example/kotlinksp/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/kotlinksp/view/uicontroller/PetaNavigasi.kt
@@ -4,17 +4,24 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.kotlinksp.view.DetailSiswaScreen
+import com.example.kotlinksp.view.EditSiswaScreen
 import com.example.kotlinksp.view.EntrySiswaScreen
 import com.example.kotlinksp.view.HomeScreen
-import com.example.kotlinksp.view.route.DestinasiHome
+import com.example.kotlinksp.view.route.DestinasiDetailSiswa
+import com.example.kotlinksp.view.route.DestinasiDetailSiswa.itemIdArg
+import com.example.kotlinksp.view.route.DestinasiEditSiswa
 import com.example.kotlinksp.view.route.DestinasiEntry
+import com.example.kotlinksp.view.route.DestinasiHome
+
 
 @Composable
-fun SiswaApp(navController: NavHostController = rememberNavController(),
-             modifier: Modifier = Modifier) {
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
 
@@ -23,21 +30,49 @@ fun SiswaApp(navController: NavHostController = rememberNavController(),
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = Modifier
-    ) {
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = { navController.navigate("${DestinasiDetailSiswa.route}/${it}") },
+
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+
+        composable(
+            route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetailSiswa.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                // ... kode lain ...
+
+                // PERBAIKAN DI SINI: Gunakan DestinasiEditSiswa
+                navigateToEditItem = { navController.navigate("${DestinasiEditSiswa.route}/$it") },
+
+                navigateBack = { navController.navigateUp() }
             )
         }
 
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = { navController.popBackStack() }
+        composable(
+            route = DestinasiEditSiswa.routeWithArgs,
+            arguments = listOf(
+                navArgument(itemIdArg) {
+                    type = NavType.IntType
+                    defaultValue = -1 // Pilihan: Menambahkan defaultValue untuk keamanan
+                }
+            )
+        ) { backStackEntry -> // backStackEntry diperlukan jika Anda ingin mengakses argumen secara langsung di sini
+            EditSiswaScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+
             )
         }
     }
